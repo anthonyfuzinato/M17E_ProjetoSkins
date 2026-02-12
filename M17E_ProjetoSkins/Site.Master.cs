5f3dcfea62a48bce40ffe48add62f8cd121cf9c8
lealthony <anthonyfuzinato@gmail.com>
2026-02-12 12:47:56 +0000

    Adicionar arquivos de projeto.

---
+++ M17E_ProjetoSkins/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M17E_ProjetoSkins
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ M17E_ProjetoSkins/Site.Mobile.Master.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M17E_ProjetoSkins
+{
+    public partial class Site_Mobile : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
